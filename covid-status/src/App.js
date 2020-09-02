ad058cf029c8bed7569053f645aae58a90b3f08a
neeerajtk <neerajtk560@gmail.com>
2020-09-02 15:02:52 +0530

    basic routing added

--- covid-status/src/App.js
+++ covid-status/src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button} from 'react-bootstrap'
 import './App.css';
+import Header from './Components/Header';
+import Country from './Components/Country';
+import World from './Components/World';
 
 function App() {
   return (
-    <div className="App">
-        Welcome
-        <Button variant="primary">Hi</Button>
-    </div>
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/country">Country</Link> </li>
+          <li><Link to="/world">World</Link></li>
+        </ul>
+      </div>
+      <Switch>
+        <Route exact path="/"><Header/></Route>
+        <Route path="/country"><Country/></Route>
+        <Route path="/world"><World/></Route>
+      </Switch>
+    </BrowserRouter>
+
+    
   );
 }
 
