c622ce269271afb92396fdb189fff7bf5d1d86aa
neeerajtk <neerajtk560@gmail.com>
2020-09-04 23:47:28 +0530

    Updates

---
+++ precache-manifest.0a6c0b3f5cedc1e090d4b5e148a010e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d1706c49ee69c4bf9cac8d6428d08a",
+    "url": "/covid-status/index.html"
+  },
+  {
+    "revision": "b8fcd9b89711355d5ba3",
+    "url": "/covid-status/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1f2b6289ea1537de6d24",
+    "url": "/covid-status/static/css/main.40828526.chunk.css"
+  },
+  {
+    "revision": "b8fcd9b89711355d5ba3",
+    "url": "/covid-status/static/js/2.c66f34e0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid-status/static/js/2.c66f34e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2b6289ea1537de6d24",
+    "url": "/covid-status/static/js/main.3355bd41.chunk.js"
+  },
+  {
+    "revision": "5f14be2a4e563ef94abc",
+    "url": "/covid-status/static/js/runtime-main.29d26a56.js"
+  }
+]);
