d6cc362a33b4fbe69f7c6e7191192b9ac07c56c2
neeerajtk <neerajtk560@gmail.com>
2020-09-04 12:08:40 +0530

    bootstrap cards added

--- covid-status/src/Components/Country.js
+++ covid-status/src/Components/Country.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import {Card, Button} from 'react-bootstrap';
+
 
 export default function Country() {
     return (
@@ -6,6 +8,24 @@ export default function Country() {
             <div className="col-md-12">
                 <h3>India</h3>
             </div>
+            <div className="col-md-12">
+                <div className="row">
+                    <div className="col-md-3">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+        
         </div>
     )
 }
