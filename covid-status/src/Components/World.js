c0127812d99a7dbe0faf0e633aee0a92b64a47e6
neeerajtk <neerajtk560@gmail.com>
2020-09-04 22:00:23 +0530

    world api added

--- covid-status/src/Components/World.js
+++ covid-status/src/Components/World.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import {Card, Accordion, Button} from 'react-bootstrap';
 
-export default function World() {
-    return (
-        <div>
-            <h2>World Component</h2>
-        </div>
-    )
+
+
+class World extends Component {
+    constructor(props){
+        super(props);
+        this.state= {
+            globalData:{},
+            data: []
+        }
+
+    }
+
+    componentDidMount(){
+        axios.get("https://corona.lmao.ninja/v2/countries")
+        .then(response => {
+            console.log(response);
+            this.setState({
+                globalData: response.data
+            })
+            
+        })
+    }
+
+    render() {
+        return (
+            
+        )
+    }
 }
+
+export default World;
